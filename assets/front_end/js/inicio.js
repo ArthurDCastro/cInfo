96f19771c3634cdd90c57cb9139327eca187d964
Arthur Castro <arthurcastro078@gmail.com>
2018-05-05 00:23:11 -0300

    Add files via upload

---
+++ assets/front_end/js/inicio.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $(document).ready(function(){
+        $('.bxslider').bxSlider({
+            mode: 'horizontal',
+            moveSlides: 1,
+            slideMargin: 0,
+            infiniteLoop: true,
+            minSlides: 1,
+            maxSlides: 1,
+            speed: 1000,
+            auto: true,
+            responsive: true
+        });
+    });
+});
---
+++ assets/front_end/js/login.js
@@ -0,0 +1,37 @@
+$('.ui.form')
+    .form({
+        fields: {
+            email: {
+                identifier: 'email',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Por favor insira um email!'
+                    }
+                ]
+            },
+            login: {
+                identifier: 'login',
+                rules: [
+                    {
+                        prompt : '...'
+                    }
+                ]
+
+            },
+            password: {
+                identifier: 'password',
+                rules: [
+                    {
+                        type   : 'empty',
+                        prompt : 'Por favor insira uma senha'
+                    },
+                    {
+                        type   : 'minLength[4]',
+                        prompt : 'Sua senha deve ter no mínimo {ruleValue} caracteres'
+                    }
+                ]
+            }
+        }
+    })
+;
