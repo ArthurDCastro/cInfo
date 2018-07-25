d175aa3d1a8e10f89fdf7e6c27c18b76f94d2201
ohperes <peresvini2@gmail.com>
2018-07-25 16:40:33 -0300

    aasad

--- app/views/graficos.php
+++ app/views/graficos.php
@@ -1,20 +1,9 @@
 <link rel="stylesheet" href="assets/front_end/css/graficos.css">
 
 
-<div class="ui three column grid" id="article">
-    <div class="column">
-        <div class="ui segment">
-            <img>
-        </div>
-    </div>
-    <div class="column">
-        <div class="ui segment">
-            <img>
-        </div>
-    </div>
-    <div class="column">
-        <div class="ui segment">
-            <img>
-        </div>
+<div class="container" style="padding:7% 4% 4% 4%;">
+    <div class="row" style="border-radius: 5px;background-color: f2f2f2; height: 59.8%; padding: 3%;">
+        <h1>Você não tem gráficos ainda...</h1>
+        <h2>Crie seu gráfico <a href="crie">aqui</a>!</h2>
     </div>
 </div>
