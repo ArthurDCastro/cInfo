17cf062b8b6cbbf106d67866bec440638e3e04b5
ArthurDCastro <arthurcastro078@gmail.com>
2018-08-03 16:06:27 -0300

    Criação de grafico

--- app/views/perfil.php
+++ app/views/perfil.php
@@ -1,8 +1,26 @@
-<br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br>
+<br><br>
+<div class="ui container" id="article">
+    <img class="ui centered small circular image" src="assets/files/img/lazinha.jpg" >
+    <div class="ui top attached tabular menu">
+        <a class="active item">
+            Bio
+        </a>
+        <a class="item">
+            Photos
+        </a>
+    </div>
+    <div class="ui bottom attached segment">
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate, tortor quis feugiat convallis, turpis justo mollis magna, eu porta lorem metus vitae nunc. Phasellus fermentum nulla sit amet pellentesque malesuada. In tempor tortor ac lobortis pharetra. Integer augue nibh, tempor eu dui sit amet, vulputate vehicula risus. Ut elementum facilisis tristique. Praesent dignissim odio quis leo faucibus, quis convallis sapien commodo. Fusce fringilla, turpis a viverra volutpat, nisi quam tincidunt turpis, et iaculis metus arcu ac felis.
+            e nisl sodales urna accumsan, at placerat risus tempus. Curabitur dignissim massa metus, et fringilla libero pretium id. Maecenas massa nunc, facilisis at ex in, tempor fermentum erat. Sed elementum arcu et nulla viverra luctus. Cras faucibus est at augue aliquam iaculis. Praesent molestie felis id risus iaculis, eget lobortis diam dictum. Sed malesuada massa ut metus semper, vel tristique velit facilisis. Maecenas mattis diam eu ante fermentum suscipit.</p>
 
-<div class="ui container segment" id="article" style="width: 90%">
-    <img class="ui small circular image" src="assets/files/img/lazinha.jpg" style="position: absolute; margin-left: 43.15%; margin-top: -7.5%; border: 2px; border-co
-">
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vulputate, tortor quis feugiat convallis, turpis justo mollis magna, eu porta lorem metus vitae nunc. Phasellus fermentum nulla sit amet pellentesque malesuada. In tempor tortor ac lobortis pharetra. Integer augue nibh, tempor eu dui sit amet, vulputate vehicula risus. Ut elementum facilisis tristique. Praesent dignissim odio quis leo faucibus, quis convallis sapien commodo. Fusce fringilla, turpis a viverra volutpat, nisi quam tincidunt turpis, et iaculis metus arcu ac felis.
-e nisl sodales urna accumsan, at placerat risus tempus. Curabitur dignissim massa metus, et fringilla libero pretium id. Maecenas massa nunc, facilisis at ex in, tempor fermentum erat. Sed elementum arcu et nulla viverra luctus. Cras faucibus est at augue aliquam iaculis. Praesent molestie felis id risus iaculis, eget lobortis diam dictum. Sed malesuada massa ut metus semper, vel tristique velit facilisis. Maecenas mattis diam eu ante fermentum suscipit.</p>
+        </p>
+    </div>
+
+
+    <!--<img class="ui small circular image" src="assets/files/img/lazinha.jpg" style="position: absolute; margin-left: 43.15%; margin-top: -7.5%; border: 2px; border-co
+">-->
 </div>
+
+<br>
