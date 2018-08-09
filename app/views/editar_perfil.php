aade16fcdbe66c3b39dd59c879e95f536097ad99
tekislla <jplazarim@gmail.com>
2018-08-09 16:05:16 -0300

    mudanças perfil - editar perfil pronto (visual)

--- app/views/editar_perfil.php
+++ app/views/editar_perfil.php
@@ -1,63 +1,64 @@
-<h4 class="ui horizontal divider header">
-    Editar Perfil
-</h4>
-<div class="two column" style="margin-top: 2%; margin-bottom: 2%; margin-left: 5%">
-    <form action="">
-        <label><b>Nome</b></label>
-        <br>
-        <div class="ui left icon input">
-            <i class="user icon"></i>
-            <input type="text" value="João Pedro Lazarim de Souza" style="width: 100%">
-        </div>
-        <br><br>
-        <label><b>Email</b></label>
-        <br>
-        <div class="ui left icon input">
-            <i class="mail icon"></i>
-            <input type="text" value="jplazarim@gmail.com" style="width: 100%">
-        </div>
-        <br><br>
-        <label><b>Nova Senha</b></label>
-        <br>
-        <div class="ui left icon input">
-            <i class="lock icon"></i>
-            <input type="text" placeholder="*****" style="width: 100%">
-        </div>
-        <br><br><br>
-        <button name="cancelar" class="ui animated red large submit button" tabindex="0">
-            <div class="visible content">Cancelar</div>
-            <div class="hidden content">
-                <i class="cancel icon"></i>
+<div class="ui form">
+    <div class="two fields">
+        <div class=" left floated column" style="margin-top: 4%; margin-bottom: 2%; margin-left: 20%">
+            <form action="">
+                <label><b>Nome</b></label>
+                <br>
+                <div class="ui left icon input">
+                    <i class="user icon"></i>
+                    <input type="text" value="João Pedro Lazarim de Souza" style="width: 100%">
+                </div>
+                <br><br>
+                <label><b>Email</b></label>
+                <br>
+                <div class="ui left icon input">
+                    <i class="mail icon"></i>
+                    <input type="text" value="jplazarim@gmail.com" style="width: 100%">
+                </div>
+                <br><br>
+                <label><b>Nova Senha</b></label>
+                <br>
+                <div class="ui left icon input">
+                    <i class="lock icon"></i>
+                    <input type="text" placeholder="*****" style="width: 100%">
+                </div>
+                <br><br><br>
+                <button name="cancelar" class="ui animated red large submit button" tabindex="0">
+                    <div class="visible content">Cancelar</div>
+                    <div class="hidden content">
+                        <i class="cancel icon"></i>
+                    </div>
+                </button>
             </div>
-        </button>
+            <div class="right floated column" style="margin-top: 4%; margin-bottom: 2%; margin-left: 25%">
+                <label><b>Nome de Usuário</b></label>
+                <br>
+                <div class="ui left icon input">
+                    <i class="unhide icon"></i>
+                    <input type="text" value="Tekislla" style="width: 100%">
+                </div>
+                <br><br>
+                <label><b>Senha Antiga</b></label>
+                <br>
+                <div class="ui left icon input">
+                    <i class="lock icon"></i>
+                    <input type="text" placeholder="*****" style="width: 100%">
+                </div>
+                <br><br>
+                <label><b>Confirme a Senha</b></label>
+                <br>
+                <div class="ui left icon input">
+                    <i class="lock icon"></i>
+                    <input type="text" placeholder="*****" style="width: 100%">
+                </div>
+                <br><br><br>
+                <button name="salvar" class="ui animated green large submit button" tabindex="0">
+                    <div class="visible content">Salvar</div>
+                    <div class="hidden content">
+                        <i class="upload icon"></i>
+                    </div>
+                </button>
+            </form>
+        </div>
     </div>
-    <div class="two column" style="margin-top: 2%; margin-bottom: 2%; margin-left: 15%">
-        <label><b>Nome de Usuário</b></label>
-        <br>
-        <div class="ui left icon input">
-            <i class="unhide icon"></i>
-            <input type="text" value="Tekislla" style="width: 100%">
-        </div>
-        <br><br>
-        <label><b>Senha Antiga</b></label>
-        <br>
-        <div class="ui left icon input">
-            <i class="lock icon"></i>
-            <input type="text" placeholder="*****" style="width: 100%">
-        </div>
-        <br><br>
-        <label><b>Confirme a Senha</b></label>
-        <br>
-        <div class="ui left icon input">
-            <i class="lock icon"></i>
-            <input type="text" placeholder="*****" style="width: 100%">
-        </div>
-        <br><br><br>
-        <button name="salvar" class="ui animated green large submit button" tabindex="0">
-            <div class="visible content">Salvar</div>
-            <div class="hidden content">
-                <i class="upload icon"></i>
-            </div>
-        </button>
-    </form>
 </div>
